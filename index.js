f7bcdbe82497dc6b19a2e2632668d8279a472b2d
Hugo Toraya <hugotoraya6@gmail.com>
2026-01-21 12:50:42 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const HTTP = require("http");
+
+const SERVER = HTTP.createServer(requestController);
+
+SERVER.listen(4000);
+
+function requestController(){
+    console.log("Se detectó una petición al servidor.");
+};
